88239a6d9f81cad8a5c558f5e8402cfed89408fe
Vadim Krukovski <vadim.krukovski@pstlabs.by>
2024-07-21 22:19:13 +0300

    task 8.Integration with SQL Database

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://dxw4vk4okl.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "hr10jwfc9a.execute-api.eu-west-1.amazonaws.com/prod",
   import: "https://njieiqmf11.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "hr10jwfc9a.execute-api.eu-west-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
